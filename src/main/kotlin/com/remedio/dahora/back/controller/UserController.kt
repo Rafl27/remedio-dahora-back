e20f8a8975dc8f35766d7275d2fa663a985668ac
Rafael <rafael.souza@hotmart.com>
2023-02-26 14:41:39 -0300

    liberando security

--- src/main/kotlin/com/remedio/dahora/back/controller/UserController.kt
+++ src/main/kotlin/com/remedio/dahora/back/controller/UserController.kt
@@ -2,15 +2,18 @@ package com.remedio.dahora.back.controller
 
 import com.remedio.dahora.back.model.User
 import com.remedio.dahora.back.service.UserService
+import jakarta.annotation.security.PermitAll
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 //TODO: usar o Response entity no retorno das funcoes
+//TODO: o security esta bloqueando as conexoes, liberar pra tudo quando voltar
 
 @RestController
 @RequestMapping("/users")
+@PermitAll
 class UserController(val service: UserService) {
 
     @PostMapping
